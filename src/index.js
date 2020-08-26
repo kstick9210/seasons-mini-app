1ef48f09304100b211f2f327ff815d531360d4e2
Kathleen Stickel <kstick9210@outlook.com>
2020-08-26 17:12:13 -0500

    refactor geolocate to lifecycle method

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { lat: null, errorMessage: '' };
+    }
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
     
